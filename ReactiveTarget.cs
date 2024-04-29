5641c657107711b00faeb58b9eb119ceaf5d06da
nathanial <nathanial28@gmail.com>
2024-04-29 08:48:41 -0600

    added basic wandering AI

--- ReactiveTarget.cs
+++ ReactiveTarget.cs
@@ -17,6 +17,10 @@ public class ReactiveTarget : MonoBehaviour
     }
 
     public void ReactToHit() {
+        WanderingAI behaviour = GetComponent<WanderingAI>();
+        if (behaviour != null) {
+            behaviour.SetAlive(false);
+        }
         StartCoroutine(Die());
     }
 
